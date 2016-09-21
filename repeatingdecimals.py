034f89ca1ca2317f105fd8a206f5c8046b3189a4
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-09-21 14:26:37 -0400

    Updated from Brython Server: 9/21/2016 2:26:38 PM

--- repeatingdecimals.py
+++ repeatingdecimals.py
@@ -27,6 +27,11 @@ for x in list(range(1, 50)):
 
 print(n)
 
+n = string(n)
+length = n.count(9)
+
+print("Repeating numbers: " + length )
+
 #once top part works, put number n into a list with all its digits
 #sort to tell how many zeros, how many nines
 #will give number of repeating digits (# of nines), # of 0 = non repeating 
