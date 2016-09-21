b4ddbaa43aeea6a6526e5f9a5524b96f0afaeaf2
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-09-21 11:38:01 -0400

    Updated from Brython Server: 9/21/2016 11:38:01 AM

--- repeatingdecimals.py
+++ repeatingdecimals.py
@@ -8,11 +8,12 @@ print(num / d)
 for x in list(range(1, 50)):
     if n % d == 0:
         break 
-    if (n*10) % d == 0:
-        n = 10*n
-        break
-    else:
-        n = 9 + 10*n
+    for x in list(range(1, 50))
+        if (n*x*10) % d == 0:
+            n = 10*n
+            break
+        else:
+            n = 9 + 10*n
 
 print(n)
 
