cc03d08b10e780c6200b2d65c2c472801dfe48aa
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-09-21 11:13:35 -0400

    Updated from Brython Server: 9/21/2016 11:13:35 AM

--- repeatingdecimals.py
+++ repeatingdecimals.py
@@ -5,8 +5,8 @@ n = 9
 print(num / d)
 print(n % d)
 
-while n % d != 0 or n*10 % n != 0:
-    n = 9 + 10*n 
+#while n % d != 0 or n*10 % n != 0:
+    #n = 9 + 10*n 
 
 #if n % d != 0 or n % d*10 != 0:
     #for x in range(1, 50):
