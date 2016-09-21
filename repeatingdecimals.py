531be453dc5584d511bc4111da0b992c6fe48638
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-09-21 14:44:01 -0400

    Updated from Brython Server: 9/21/2016 2:44:02 PM

--- repeatingdecimals.py
+++ repeatingdecimals.py
@@ -10,17 +10,17 @@ for a in list(range(1, 10)):
     if 10**a*(num/d) % 1 == 0:
         break
    
-
-for x in list(range(1, 50)):
-    if n % d == 0:
-        break
-    else: 
-        for y in list(range(1, 10)):
-            if n*10**y % d ==0:
-                n = n*10**y
-                break
-        else:
-            n = 9 + 10*n
+else:
+    for x in list(range(1, 50)):
+        if n % d == 0:
+            break
+        else: 
+            for y in list(range(1, 10)):
+                if n*10**y % d ==0:
+                    n = n*10**y
+                    break
+            else:
+                n = 9 + 10*n
 
 
 print(n)
