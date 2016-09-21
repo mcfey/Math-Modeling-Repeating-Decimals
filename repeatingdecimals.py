7a2c84ba19ed1bbb8fb7a56eecda8ec9d2da50f9
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-09-21 10:57:56 -0400

    Updated from Brython Server: 9/21/2016 10:57:55 AM

--- repeatingdecimals.py
+++ repeatingdecimals.py
@@ -2,9 +2,13 @@ num = int(input("Numerator: "))
 d = int(input("Denominator: "))
 
 n = 9
-
-while d % n != 0 or d % n*10 != 0:
-    n = 9 + 10*n 
-    
 print(num / d)
+
+#while d % n != 0 or d % n*10 != 0:
+    #n = 9 + 10*n 
+
+if d % n != 0 or d % n*10 != 0:
+    n = 9 + 10*9
+
 print(n)
+
